ef4e45265a6e3a632246935c90ee563147e83acd
Mike506 <mvegar94@gmail.com>
2024-06-22 19:29:54 -0600

    first commit

--- src/components/SocialLinks/index.jsx
+++ src/components/SocialLinks/index.jsx
@@ -2,7 +2,6 @@ import './style.css';
 import { IoLogoGithub, IoIosMail } from "react-icons/io";
 import { IoLogoLinkedin } from "react-icons/io5";
 import { TbFileCv } from "react-icons/tb";
-import pdf from '%PUBLIC_URL%/resume.pdf';
 
 const SocialLinks = ({ githubURL, linkedinURL, mailTo, resumeURL }) => {
   return(
@@ -10,7 +9,7 @@ const SocialLinks = ({ githubURL, linkedinURL, mailTo, resumeURL }) => {
       <a href={githubURL} target="_blank" rel="noreferrer"><IoLogoGithub className="social-icon" /></a>
       <a href={linkedinURL}  target="_blank" rel="noreferrer"><IoLogoLinkedin className="social-icon" /></a>
       <a href={mailTo} target="_blank" rel="noreferrer"><IoIosMail className="social-icon mail" /></a>
-      <a href={pdf} target="_blank" rel="noreferrer"><TbFileCv className="social-icon"/></a>
+      <a href={resumeURL} target="_blank" rel="noreferrer"><TbFileCv className="social-icon"/></a>
     </div>
   )
 }
